f36fbf46c42c8fc538ba53e0b4209cfb8900db70
milllkkkyyy <64578065+milllkkkyyy@users.noreply.github.com>
2022-11-11 10:15:54 -0500

    Changed Hotdog Minigame to work with new input system

--- Assets/Scripts/HotdogHandler.cs
+++ Assets/Scripts/HotdogHandler.cs
@@ -2,41 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HotdogHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject hotdogGO;
-    Hotdog hotdog;
     [SerializeField] Transform spawnLocation;
+    Hotdog hotdog;
     GameObject clone;
-    
-    public void CreateHotdog()
+
+    int amountEaten; 
+
+    void Start()
     {
-       clone = Instantiate(hotdogGO, spawnLocation);
+        CreateHotdog();
+    }
+
+    void Update()
+    {
+        if (hotdog.IsFinished())
+        {
+            amountEaten++;
+            DestroyHotdog();
+            Debug.Log("Ate 1 Hotdog.");
+        }
+    }
+
+    void CreateHotdog()
+    {
+        clone = Instantiate(hotdogGO, spawnLocation);
         hotdog = clone.GetComponent<Hotdog>();
-
     }
 
-    public KeyCode GetCurrentButton()
-    {
-        return hotdog.GetButton();
-    }
-
-    public void Bite()
-    {
-        hotdog.Bite();
-    }
-
-    public bool IsFinished()
-    {
-        return hotdog.IsFinished();
-    }
-
-    public void DestroyHotdog()
+    void DestroyHotdog()
     {
         Destroy(clone);
         CreateHotdog();
     }
-    
+
+    public int GetAmountEaten()
+    {
+        return amountEaten;
+    }
 }
