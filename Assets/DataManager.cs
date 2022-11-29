91dd0cb3904b119595ba6a8d6eb6e53cecbbb1a0
milllkkkyyy <64578065+milllkkkyyy@users.noreply.github.com>
2022-11-28 19:43:31 -0500

    Switched How Intensity works

--- Assets/DataManager.cs
+++ Assets/DataManager.cs
@@ -6,5 +6,7 @@ public static class DataManager
 {
     public static SkyMinigame sky = new SkyMinigame();
 
+    public static HighnoonMinigame high = new HighnoonMinigame();
 
+    public static int difficulty = 0;
 }
--- Assets/HighnoonMinigame.cs
+++ Assets/HighnoonMinigame.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class HighnoonMinigame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Pointer
     {
-        
+        Vector3 position;
+        Quaternion rotation;
+
+        public void SaveTransform(Transform transform)
+        {
+            position = transform.position;
+            rotation = transform.rotation;
+        }
+
+        public void LoadTransform(Transform transform)
+        {
+            transform.position = position;
+            transform.rotation = rotation;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Data
     {
-        
+        int score;
+
+        public void SetScore(int score)
+        {
+            this.score = score;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+    }
+
+    Pointer pointer = new Pointer();
+    Data data = new Data();
+
+    public void SetPointer(Pointer pointer)
+    {
+        this.pointer = pointer;
+    }
+
+    public Pointer GetPointer()
+    {
+        return this.pointer;
+    }
+
+    public void SetData(Data data)
+    {
+        this.data = data;
+    }
+
+    public Data GetData()
+    {
+        return this.data;
     }
 }
