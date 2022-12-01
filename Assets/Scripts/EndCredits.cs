682d03cf2d7d6122ac4c6995d7684cea31c31a9f
SwanPrince2 <ethanreed2000@gmail.com>
2022-12-01 02:49:11 -0500

    Credits Finished

--- Assets/Scripts/EndCredits.cs
+++ Assets/Scripts/EndCredits.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,22 +7,27 @@ using UnityEngine;
 public class EndCredits : MonoBehaviour
 {
     RectTransform rectTransform;
+    Boolean creditsEnded;
     private void Start()
     {
+        creditsEnded = false;
         //Fetch the RectTransform from the GameObject
         rectTransform = GetComponent<RectTransform>();
     }
     void Update()
     {
-        if (rectTransform.position.y >= 460)
+        if (rectTransform.position.y >= 12 && !creditsEnded)
         {
             //return to menu
             TelevisionManager.instance.ChangeScene("MainMenu", "Change_Start");
+            creditsEnded = true;
+            
 
         }
         else 
         {
             transform.Translate(Vector2.up * 1.2f * Time.deltaTime);
+            Debug.Log(rectTransform.position.y);
         }
 
     }
